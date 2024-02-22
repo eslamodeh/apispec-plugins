2030e486e00ab59e11ec3af234e7cbb54548cbd4
Eslam Odeh <eslamodeh@outlook.com>
2024-02-22 15:21:31 +0000

    remove marsh

--- src/apispec_plugins/base/mixin.py
+++ src/apispec_plugins/base/mixin.py
@@ -1,8 +1,4 @@
-from dataclasses import MISSING, fields
-from typing import get_args
 
-from apispec import APISpec
-from apispec.ext.marshmallow.openapi import OpenAPIConverter
 from apispec_plugins.base.registry import Registry
 
 
@@ -23,24 +19,24 @@ class DataclassSchemaMixin:
             return model.schema()
 
         # or fallback to marshmallow resolver
-        return cls.marshmallow_resolver()
+        #  return cls.marshmallow_resolver()
 
-    @classmethod
-    def marshmallow_resolver(cls, openapi_version="2.0"):
-        from apispec.ext.marshmallow.openapi import marshmallow as ma
+    #  @classmethod
+    #  def marshmallow_resolver(cls, openapi_version="2.0"):
+    #      from apispec.ext.marshmallow.openapi import marshmallow as ma
 
-        openapi_converter = OpenAPIConverter(
-            openapi_version=openapi_version,
-            schema_name_resolver=lambda f: None,
-            spec=APISpec("", "", openapi_version),
-        )
+    #      openapi_converter = OpenAPIConverter(
+    #          openapi_version=openapi_version,
+    #          schema_name_resolver=lambda f: None,
+    #          spec=APISpec("", "", openapi_version),
+    #      )
 
-        def schema_type(t):
-            return ma.Schema.TYPE_MAPPING[next(iter(get_args(t)), t)]
+    #      def schema_type(t):
+    #          return ma.Schema.TYPE_MAPPING[next(iter(get_args(t)), t)]
 
-        schema_dict = {
-            f.name: schema_type(f.type)(data_key=f.name, required=f.default is MISSING)
-            for f in fields(cls)
-        }
-        schema = ma.Schema.from_dict(schema_dict)
-        return openapi_converter.schema2jsonschema(schema)
+    #      schema_dict = {
+    #          f.name: schema_type(f.type)(data_key=f.name, required=f.default is MISSING)
+    #          for f in fields(cls)
+    #      }
+    #      schema = ma.Schema.from_dict(schema_dict)
+    #      return openapi_converter.schema2jsonschema(schema)
